c739daeeb941ec6d19b0d27343822b506000ea48
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-05-22 15:09:38 -0700

    got more usability for the add feature

--- module-4-assets/taskinator.js
+++ module-4-assets/taskinator.js
@@ -1,16 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-var formEl = document.querySelector("#task-form");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function(event) {
+// create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
 
-    event.preventDefault();
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+// give it a class name
+taskInfoEl.className = "task-info";
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
 
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-};
+listItemEl.appendChild(taskInfoEl);
 
-
-formEl.addEventListener("submit", createTaskHandler);
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
